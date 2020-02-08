1421f602919df10f66874633cb56da27a13c6335
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-08 18:22:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -37,7 +37,6 @@ def movies_with_director_key(name, movies_collection)
   aoh = []
   movie_index = 0
   while movie_index < movies_collection.length do
-
     director_name = movie_with_director_name(name, movies_collection)
     aoh << director_name
     movie_index += 1
